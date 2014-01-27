cf68ad36d0305fe322fcad46334b7e8d217a37b9
lrpnobody <lrpnobody@gmail.com>
2014-01-26 19:18:20 -0500

    add files

---
+++ test/test/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.ReadKey();
+        }
+
+
+    }
+}
